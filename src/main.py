67312f854c38c2e9e924e2f8106f29b5ba474fd8
Alexandr Gromov <alex.gromov.2410@gmail.com>
2025-08-05 19:02:17 -0700

    Parsing sample image as command line args
    - Scaled down original image and changed and added alpha channel

--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
-def main():
-    print("Welcome to ITOT-AI!")
+import numpy as np
+
+from argparse import ArgumentParser
+from PIL import Image
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("--image-path", required=True)
+    return parser.parse_args()
+
+def main(image_path):
+    img = Image.open(image_path).convert(mode='L')
+    img = np.array(img)
+    print(img.shape, img.dtype)
 
 if __name__ == "__main__":
-    main()
+    main(**vars(parse_args()))
